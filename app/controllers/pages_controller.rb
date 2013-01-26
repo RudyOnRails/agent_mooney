6569a12ac9ae3a44a4e85b30068337a2b52d219e
Kevin Musiorski <rudyonrails@gmail.com>
2013-01-26 17:55:19 -0600

    init

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def music
+  end
+
+  def shows
+  end
+
+  def news
+  end
+
+  def faq
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgentMooney::Application.config.secret_token = '7c6672a1a594d7aee2b43ed1b7e5f27ad2ba344396926bea568d4d248ac9fa5bb73fa765173e53a0e711c9ecae54db59e685380e3b537a2a91032d6a6a3db8ed'
